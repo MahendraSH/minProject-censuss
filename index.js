eb656347cafcf30168c1f8b62fd9b2fb8db27ee6
MahendrSH <maahi2001sh@gmail.com>
2023-04-22 17:24:47 +0530

    min project version -1

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use('/',require('./routes/routesLogin'))
 // console.log(req.body);
 // });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
     console.log(' listening on port 3000!')
 });
