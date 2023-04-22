a6523f0ce416631313ed895ce2b118046e4589bd
MahendrSH <maahi2001sh@gmail.com>
2023-04-22 22:55:41 +0530

    min project version -1

--- routes/routesLogin.js
+++ routes/routesLogin.js
@@ -1,18 +1,14 @@
-const express =require(`express`)
-const con = require('../DB/dbconnect')
+const express = require(`express`)
 const route = express.Router();
 
-route.get('/home', (req, res) => {
-    res.render('index');
-  });
-route.get('/about', (req, res) => {
-    res.render('about');
-  });
-route.get('/gallery', (req, res) => {
-    res.render('gallery');
-  });
 route.get('/', (req, res) => {
   res.render('index');
-  });
-  
-module.exports=route;
+});
+route.get('/about', (req, res) => {
+  res.render('about');
+});
+route.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
+
+module.exports = route;
