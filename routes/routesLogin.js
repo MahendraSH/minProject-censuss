ece30ae252b8b69bbcbb886e0512cd3f3fd4a74d
MahendrSH <maahi2001sh@gmail.com>
2023-04-24 14:36:05 +0530

    added iframe for 2001 and preduction

--- routes/routesLogin.js
+++ routes/routesLogin.js
@@ -11,4 +11,12 @@ route.get('/gallery', (req, res) => {
   res.render('gallery');
 });
 
+route.get('/2011', (req,res) =>{
+  res.render('2011');
+})
+
+route.get('/prediction', ( req,res)=>{
+  res.render('prediction')
+})
+
 module.exports = route;
